b7aa6c192bdb6e1836ceef2b93237fadf89c8045
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-02-07 02:31:30 +0530

    Solve roman-numerals in Javascript

---
+++ Solutions/javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRoman = (num) => {
+  const romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let result = "";
+  for (const key in romanNumerals) {
+    while (num >= romanNumerals[key]) {
+      result += key;
+      num -= romanNumerals[key];
+    }
+  }
+  return result;
+};
