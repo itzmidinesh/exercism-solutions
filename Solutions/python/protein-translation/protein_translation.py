cdf9a7a7d51861c17373c1fdccc6c0c2a3731a96
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-02-13 17:28:30 +0530

    Solve protein-translation in Python

---
+++ Solutions/python/protein-translation/protein_translation.py
@@ -0,0 +1,30 @@
+catalogue = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP",
+}
+
+
+def proteins(strand):
+    proteins = []
+    for i in range(0, len(strand), 3):
+        codon = strand[i : i + 3]
+        if catalogue[codon] == "STOP":
+            break
+        else:
+            proteins.append(catalogue[codon])
+    return proteins
