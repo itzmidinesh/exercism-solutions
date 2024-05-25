777066ca08f9ed44aef6b3a6dbd2147e7a56db69
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-05-25 14:43:24 +0530

    Solve dnd-character in Python

---
+++ Solutions/python/dnd-character/dnd_character.py
@@ -0,0 +1,20 @@
+import random
+
+
+def modifier(score):
+    return (score - 10) // 2
+
+
+class Character:
+    def __init__(self):
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        rolls = [random.randint(1, 6) for _ in range(4)]
+        return sum(sorted(rolls)[1:])
